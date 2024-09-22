12d93837e76db682a30b15ccc11c3c06a0afd6fd
bhuvaneswariiiii <138887277+bhuvaneswariiiii@users.noreply.github.com>
2024-09-22 22:11:10 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,9 @@ const Hero = () => {
           className="bg-gradient-to-r from-green-400 to-blue-500 text-white
           transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
         >
-          Contact With Me
+          <a href="https://www.linkedin.com/in/bhuvaneswari-kalavakollu-9bb774247/">
+              Connect Me
+            </a>
         </button>
         <a
           href="/sd.pdf"
